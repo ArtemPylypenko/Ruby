4117bee258d8bed40d804b59b38bb4eceed4f0ba
ArtemPylypenko <numb432hz@gmail.com>
2022-10-28 21:05:24 +0300

    Reformat Code

--- lab6/lab6.rb
+++ lab6/lab6.rb
@@ -15,7 +15,7 @@ end
 class Teacher
   attr_accessor :name, :studentsMap, :isAttend
 
-  def initialize(name,students)
+  def initialize(name, students)
     @name = name
     @studentsMap = students
   end
@@ -23,90 +23,93 @@ class Teacher
   def inspect
     "Name : #{@name}\nAttend : #{@attend}\nIs attend : #{@isAttend}"
   end
+
   def gradeToTen()
     @studentsMap.each do |student|
-      if(student.attend == "GRADE_TO_TEN")
+      if (student.attend == "GRADE_TO_TEN")
         print(student.inspect + "\n")
-    end
+      end
 
     end
   end
+
   def gradeNotToTen()
     @studentsMap.each do |student|
-      if(student.attend != "GRADE_TO_TEN")
+      if (student.attend != "GRADE_TO_TEN")
         print(student.inspect + "\n")
       end
 
     end
   end
+
   def failed()
     @studentsMap.each do |student|
-      if(student.isAttend == false )
+      if (student.isAttend == false)
         print(student.inspect + "\n")
       end
 
     end
   end
+
   def goToTen()
     count = 0
     @studentsMap.each do |student|
-      if(student.isAttend == false || student.attend == "GRADE_TO_TEN")
-        count = count+1
+      if (student.isAttend == false || student.attend == "GRADE_TO_TEN")
+        count = count + 1
       end
     end
     count
   end
+
   def countOfClasses()
     studentsCount = 0
     @studentsMap.each do |student|
-      if(student.isAttend == false || student.attend == "GRADE_TO_TEN")
-        studentsCount = studentsCount+1
+      if (student.isAttend == false || student.attend == "GRADE_TO_TEN")
+        studentsCount = studentsCount + 1
       end
     end
-    count = studentsCount/25
-    if(studentsCount%25 != 0 )
-      count+=1
+    count = studentsCount / 25
+    if (studentsCount % 25 != 0)
+      count += 1
     end
     count
   end
 
 end
 
-
-
 students = [
-  Student.new("Saches","PTU",true),
-  Student.new("Sonia","ZNU",false ),
-  Student.new("Mary","KNU",true),
-  Student.new("Kirill","PTU",true),
-  Student.new("Katia","SRY",false),
-  Student.new("Andre","GRADE_TO_TEN",true),
-  Student.new("Sasha","GRADE_TO_TEN",true),
-  Student.new("Dmitro","GRADE_TO_TEN",true),
-  Student.new("Nikita","GRADE_TO_TEN",true),
-  Student.new("Andre","GRADE_TO_TEN",true),
-  Student.new("Nikita","GRADE_TO_TEN",true),
-  Student.new("Dmitro","GRADE_TO_TEN",true),
-  Student.new("Sasha","GRADE_TO_TEN",true),
-  Student.new("Dmitro","GRADE_TO_TEN",true),
-  Student.new("Nikita","GRADE_TO_TEN",true),
-  Student.new("Sasha","GRADE_TO_TEN",true),
-  Student.new("Nikita","GRADE_TO_TEN",true),
-  Student.new("Andre","GRADE_TO_TEN",true),
-  Student.new("Nikita","GRADE_TO_TEN",true),
-  Student.new("Dmitro","GRADE_TO_TEN",true),
-  Student.new("Sasha","GRADE_TO_TEN",true),
-  Student.new("Dmitro","GRADE_TO_TEN",true),
-  Student.new("Nikita","GRADE_TO_TEN",true),
-  Student.new("Dmitro","GRADE_TO_TEN",true),
-  Student.new("Andre","GRADE_TO_TEN",true),
-  Student.new("Dmitro","GRADE_TO_TEN",true),
-  Student.new("Nikita","GRADE_TO_TEN",true),
-  Student.new("Andre","GRADE_TO_TEN",true),
-  Student.new("Sasha","GRADE_TO_TEN",true)
+  Student.new("Saches", "PTU", true),
+  Student.new("Sonia", "ZNU", false),
+  Student.new("Mary", "KNU", true),
+  Student.new("Kirill", "PTU", true),
+  Student.new("Katia", "SRY", false),
+  Student.new("Andre", "GRADE_TO_TEN", true),
+  Student.new("Sasha", "GRADE_TO_TEN", true),
+  Student.new("Dmitro", "GRADE_TO_TEN", true),
+  Student.new("Nikita", "GRADE_TO_TEN", true),
+  Student.new("Andre", "GRADE_TO_TEN", true),
+  Student.new("Nikita", "GRADE_TO_TEN", true),
+  Student.new("Dmitro", "GRADE_TO_TEN", true),
+  Student.new("Sasha", "GRADE_TO_TEN", true),
+  Student.new("Dmitro", "GRADE_TO_TEN", true),
+  Student.new("Nikita", "GRADE_TO_TEN", true),
+  Student.new("Sasha", "GRADE_TO_TEN", true),
+  Student.new("Nikita", "GRADE_TO_TEN", true),
+  Student.new("Andre", "GRADE_TO_TEN", true),
+  Student.new("Nikita", "GRADE_TO_TEN", true),
+  Student.new("Dmitro", "GRADE_TO_TEN", true),
+  Student.new("Sasha", "GRADE_TO_TEN", true),
+  Student.new("Dmitro", "GRADE_TO_TEN", true),
+  Student.new("Nikita", "GRADE_TO_TEN", true),
+  Student.new("Dmitro", "GRADE_TO_TEN", true),
+  Student.new("Andre", "GRADE_TO_TEN", true),
+  Student.new("Dmitro", "GRADE_TO_TEN", true),
+  Student.new("Nikita", "GRADE_TO_TEN", true),
+  Student.new("Andre", "GRADE_TO_TEN", true),
+  Student.new("Sasha", "GRADE_TO_TEN", true)
 ]
 
-teacher1 = Teacher.new("Sucker",students)
+teacher1 = Teacher.new("Sucker", students)
 
-print(teacher1.goToTen.to_s  + "\n")
+print(teacher1.goToTen.to_s + "\n")
 print(teacher1.countOfClasses)
